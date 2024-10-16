fc5d1a986cbdc449ab3a919aab7440272739adc7
AsierCode <asierngcode@gmail.com>
2024-10-16 21:32:49 +0200

    Upload 16-10-24

---
+++ Reloj/script.js
@@ -0,0 +1,54 @@
+// Selección de los elementos del reloj
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+// Días y meses para mostrar en la fecha
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// Función para cambiar entre modo claro y oscuro
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light mode';
+    }
+});
+
+// Función para actualizar la hora y la fecha en tiempo real
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    // Establece la rotación de las agujas del reloj
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+
+    // Actualiza el texto de la hora y la fecha
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+// Función para mapear un rango de números a otro rango
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+// Inicializa el reloj y actualízalo cada segundo
+setTime();
+setInterval(setTime, 1000);
